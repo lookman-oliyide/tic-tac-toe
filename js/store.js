bce35b58cbacc845b5600866cc6656d21bbfe8d8
Lookman Oliyide <69759909+lookman-oliyide@users.noreply.github.com>
2023-10-15 00:02:55 +0100

    MVC refactor

---
+++ js/store.js
@@ -0,0 +1,142 @@
+const initialValue = {
+    currentGameMoves: [],
+    gameHistory: {
+        currentRoundGames: [],
+        allGames: [],
+    },
+};
+
+export default class Store {
+
+    #state = initialValue;
+
+    constructor(players) {
+        this.players = players;
+    }
+
+    get gameStats() {
+        const state = this.#getState();
+
+        return {
+            playerStats: this.players.map((player) => {
+                const wins = state.gameHistory.currentRoundGames.filter( // optional chaining operator
+                    (game) => game.status.winner?.id === player.id
+                ).length; // filter by player id, the games where player id  = winner
+                    
+                
+                return {
+                    ...player,
+                    wins
+                }
+            }),
+
+            ties: state.gameHistory.currentRoundGames.filter(
+                game => game.status.winner === null
+            ).length,
+        };
+    }
+
+    get game() { 
+        const state = this.#getState();
+
+        const currentPlayer = this.players[state.currentGameMoves.length % 2]; // 0 || 1
+
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [3, 5, 7],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        let winner = null;
+
+        for (const player of this.players) { // loop through players
+            const selectedCellIds = state.currentGameMoves
+                .filter((move) => move.player.id === player.id)
+                .map((move) => move.cellId)
+            
+            for (const pattern of winningPatterns) {
+                if (pattern.every((value) => selectedCellIds.includes(value))) {
+                    winner = player;
+                }
+            }
+        }
+        
+        return {
+            gameMoves: state.currentGameMoves,
+            currentPlayer,
+            status: {
+                isComplete: winner != null || state.currentGameMoves.length === 9,
+                winner
+            },
+        };
+    }
+    // added getter so the function is called at runtime
+    // and won't need to be recalled in module
+
+    
+    
+    playerMove(cellId) {        
+        const stateClone = structuredClone(this.#getState());// Created so I don't mutate state
+        
+        stateClone.currentGameMoves.push({
+            cellId,
+            player: this.game.currentPlayer,
+        }); // update state clone
+        
+        this.#saveState(stateClone); // save updated state
+    }
+
+    reset() {
+        const stateClone = structuredClone(this.#getState());
+        const { status, gameMoves } = this.game;
+
+        if (status.isComplete) {
+            stateClone.gameHistory.currentRoundGames.push({
+                gameMoves,
+                status
+            })
+        } // if game is complete, set push gamesMoves and status to stateClone
+
+        stateClone.currentGameMoves = [];// then empty gameMoves array
+
+        this.#saveState(stateClone);
+    }
+
+    newGame() {
+        this.reset();
+
+        const stateClone = structuredClone(this.#getState());
+        stateClone.gameHistory.allGames.push(...stateClone.gameHistory.currentRoundGames); //push current roud data to all games
+        stateClone.gameHistory.currentRoundGames = [];// empty current round games
+
+        this.#saveState(stateClone);
+    }
+    
+    #getState() {
+        return this.#state
+    }
+
+    #saveState(stateOrFn) {
+        const prevState = this.#getState();
+        
+        let newState;
+
+        switch (typeof stateOrFn) {
+            case "function":
+                newState = stateOrFn(prevState);
+                break;
+            case "object":
+                newState = stateOrFn;
+                break;
+            default:
+                throw new Error("Invalid argument passed into saveState")
+        }
+
+        this.#state = newState;
+    }
+}
---
+++ js/view.js
@@ -0,0 +1,184 @@
+export default class View {
+
+    $ = {};
+    $$ = {};
+
+    constructor(){
+        this.$.menu = this.#qs('[data-id="menu"]');
+        this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+        this.$.menuItems = this.#qs('[data-id="menu-items"]');
+        this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+        this.$.newGameBtn = this.#qs('[data-id="new-game-btn"]');
+        this.$.modal = this.#qs('[data-id="modal"]');
+        this.$.modalIcon = this.#qs('[data-id="modal-icon"]');
+        this.$.modalText = this.#qs('[data-id="modal-text"]');
+        this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+        this.$.turn = this.#qs('[data-id="turn"]');
+        this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+        this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+        this.$.ties = this.#qs('[data-id="ties"]');
+        
+        this.$$.cells = this.#qsAll('[data-id="cell"]');
+
+        // (UI-only event listeners) The event lsiteners that don't control state can be registered here
+        this.$.menuBtn.addEventListener("click", (event) => {
+            this.#toggleMenu()
+        });
+
+        this.$.menuBtn.addEventListener("mouseover", (event) => {
+            this.#addHover()
+        });
+
+        this.$.menuBtn.addEventListener("mouseout", (event) => {
+            this.#removeHover()
+        })
+
+    }
+
+    /**
+     * Register all event Listeners
+     */
+
+    // To handle event listeners that control state within the controller instead of the view
+    // They will take a handler callback functions
+    bindResetEvent(handler) {// regsiter all buttons that perform this event
+        this.$.resetBtn.addEventListener("click", handler);
+        this.$.modalBtn.addEventListener("click", handler);
+    }
+
+    bindNewGameEvent(handler) {
+        this.$.newGameBtn.addEventListener("click", handler);
+    }
+
+    bindPlayerMoveEvent(handler) {
+        this.$$.cells.forEach((cell) => {
+            cell.addEventListener("click", () => handler(cell));
+        });
+    }
+
+    /**
+     * DOM helper methods
+     */
+
+    openModal(message) {
+        this.$.modal.classList.remove("hidden");
+        this.$.modalText.innerText = message;
+    }
+
+    #closeModal() {
+        this.$.modal.classList.add("hidden");
+        this.$.modalIcon.replaceChildren();
+    }
+
+    closeAll() {
+        this.#closeModal();
+        this.#closeMenu();
+    }
+
+    clearGameMoves() {
+        this.$$.cells.forEach((cell) => {
+            cell.replaceChildren();
+        });
+    }
+
+    handleModalIcon(winner, players) {
+        const x = document.createElement('i');
+        x.classList
+            .add("fa-solid",
+                players[0].iconClass,
+                players[0].colorClass);
+        const o = document.createElement('i');
+        o.classList
+            .add("fa-solid",
+                players[1].iconClass,
+                players[1].colorClass);
+    
+
+        if (winner) {
+            const icon = document.createElement("i");
+            icon.classList.add("fa-solid",
+                winner.iconClass,
+                winner.colorClass);
+        
+            this.$.modalIcon.appendChild(icon);    
+        } else {
+            this.$.modalIcon.appendChild(x);
+            this.$.modalIcon.appendChild(o);
+        }   
+    }
+
+    updateScoreBoard(p1Wins, p2Wins, ties) {
+        this.$.p1Wins.innerText = `${p1Wins} wins`;
+        this.$.p2Wins.innerText = `${p2Wins} wins`;
+        this.$.ties.innerText = `${ties}`;
+    }
+
+    #toggleMenu() {
+        this.$.menuItems.classList.toggle("hidden");
+
+        const icon = this.$.menuBtn.querySelector("i");
+        icon.classList.toggle("fa-chevron-down");
+        icon.classList.toggle("fa-chevron-up");
+    }
+
+    #closeMenu() {
+        this.$.menuItems.classList.add("hidden");
+
+        const icon = this.$.menuBtn.querySelector("i");
+        icon.classList.add("fa-chevron-down");
+        icon.classList.remove("fa-chevron-up");
+    }
+
+    #addHover() {
+        this.$.menuBtn.classList.add("border");
+    }
+
+    #removeHover() {
+        this.$.menuBtn.classList.remove("border");
+    }
+
+    // function to handle error with invalid selector
+    // allows us to set parent as argument but defaults to "document"
+    #qs(selector, parent) {
+        const el = parent
+            ? parent.querySelector(selector)
+            : document.querySelector(selector);
+
+        if (!el) throw new Error(`Could not find ${selector}`);
+
+        return el;
+    }
+
+    #qsAll(selector) {
+        const elList = document.querySelectorAll(selector);
+
+        if (!elList) throw new Error(`Could not find ${selector}`);
+        return elList;
+    }
+
+    setTurnIndicator(player) {
+        const icon = document.createElement("i");
+        const label = document.createElement("p");
+
+        icon.classList.add("fa-solid",
+            player.iconClass,
+            player.colorClass);
+        label.classList.add(player.colorClass);
+        label.innerText = `${player.name}, your turn!`;
+        // label.innerText = `Your turn!`;
+
+
+        this.$.turn.replaceChildren(icon, label);
+    }
+
+    handlePlayerMove(clickedCell, player) {
+        const icon = document.createElement("i");
+        
+        icon.classList.add("fa-solid",
+            player.iconClass,
+            player.colorClass);
+        clickedCell.replaceChildren(icon);
+    }
+
+    
+}
